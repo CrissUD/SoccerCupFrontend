93f9c0904661c77d88c26598d1be16a60b7127ab
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 09:09:17 -0500

    Primera version proyecto frontend soccerCup

---
+++ soccerCupApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TournamentDataComponent } from './components/tournament-data/tournament-data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TournamentDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
