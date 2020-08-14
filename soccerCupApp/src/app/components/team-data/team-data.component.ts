b791ac791508ff395391c5e29b52124420378e8a
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 10:41:19 -0500

    Se crea modelo de equipo

--- soccerCupApp/src/app/components/team-data/team-data.component.ts
+++ soccerCupApp/src/app/components/team-data/team-data.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import swal from 'sweetalert2';
+import { TeamService } from 'src/app/services/team.service';
+import { Team } from 'src/app/models/Team';
 
 @Component({
   selector: 'app-team-data',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamDataComponent implements OnInit {
 
-  constructor() { }
+  @Output()
+  estado = new EventEmitter<boolean>();
+  
+  form: FormGroup;
+  team: Team;
+
+  constructor(private fb: FormBuilder, teamService: TeamService) { 
+    this.team = new Team();
+    this.createForm();
+  }
 
   ngOnInit(): void {
   }
 
+  createForm(){
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      coach: ['', Validators.required],
+      leader: ['', Validators.required]
+    });
+  }
+
+  validationField(field){
+    return this.form.get(field).invalid && this.form.get(field).touched
+  }
+
+  saveTeams(){
+    
+  }
 }
---
+++ soccerCupApp/src/app/models/Team.ts
@@ -0,0 +1,14 @@
+export class Team{
+    id_team: string;
+    name: string;
+    coach: string;
+    leader: number;
+    gamesPlayed: number;
+    wonMatches: number;
+    lostMatches: number;
+    tiedMatches: number;
+    totalPoints: number;
+    goalsScored: number;
+    goalsConceded: number;
+    goalDiference: number;
+}
